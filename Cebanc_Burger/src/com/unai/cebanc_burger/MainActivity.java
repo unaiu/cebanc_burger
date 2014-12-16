3379dd0c59182458e2529e7ac1fe429b4382dbc4
Srivero87 <srivero87@gmail.com>
2014-12-16 09:21:11 +0100

    añadir boton siguente

--- Cebanc_Burger/src/com/unai/cebanc_burger/MainActivity.java
+++ Cebanc_Burger/src/com/unai/cebanc_burger/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 public class MainActivity extends ActionBarActivity {
 	
 	private Button salir;
+	private Button siguiente;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
